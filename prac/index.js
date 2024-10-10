503b1fc7c1ba9eb9875a2ef71676784b508e277f
Omangqueeneth <omangqueen@gmail.com>
2024-10-10 11:29:45 +0100

    first commit

---
+++ prac/index.js
@@ -0,0 +1,18 @@
+function increment() {
+    console.log("i was there")
+}
+increment()
+
+let name = "flora"
+console.log (name)
+
+let count = 100
+console.log (count)
+console.log (count + 50)
+console.log (count - 20)
+
+function sell() {
+    console.log (50)
+}
+
+sell()
